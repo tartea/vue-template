3ceb9b8e6ad11660d3d029fc59baee5d5c42c8bb
tartea <1262466460@qq.com>
2023-05-21 10:16:33 +0800

    初始化项目

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>HOME</div>
+</template>
+<script>
+import { computed, onMounted, ref, watch } from "vue";
+export default {
+  name: "Home",
+  props: {},
+  setup() {
+    const movePosition = () => {};
+    onMounted(() => {
+      console.log(`mounted`);
+    });
+    return {
+      movePosition,
+    };
+  },
+};
+</script>
